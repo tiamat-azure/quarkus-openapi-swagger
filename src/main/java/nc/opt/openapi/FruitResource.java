2d0818187d79f6f44c4df0f9ca9d126dbf746e44
Tiamat <tiamat.azure@gmail.com>
2019-11-09 19:29:44 +1100

    Add REST endpoints + test

--- src/main/java/nc/opt/openapi/FruitResource.java
+++ src/main/java/nc/opt/openapi/FruitResource.java
@@ -1,16 +1,39 @@
 package nc.opt.openapi;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Set;
+
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
-@Path("/C:/Program Files/Git/fruits")
+
+@Path("/fruits")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class FruitResource {
 
+    private Set<Fruit> fruits = Collections.newSetFromMap(Collections.synchronizedMap(new LinkedHashMap<>()));
+
+    public FruitResource() {
+        fruits.add(new Fruit("Apple", "Winter fruit"));
+        fruits.add(new Fruit("Pineapple", "Tropical fruit"));
+    }
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
+    public Set<Fruit> list() {
+        return fruits;
+    }
+
+    @POST
+    public Set<Fruit> add(Fruit fruit) {
+        fruits.add(fruit);
+        return fruits;
+    }
+
+    @DELETE
+    public Set<Fruit> delete(Fruit fruit) {
+        fruits.removeIf(existingFruit -> existingFruit.name.contentEquals(fruit.name));
+        return fruits;
     }
 }
